bcb55deab4f6dadaaf7c891d919c0bdc3db93c77
silivestir <84616833+silivestir@users.noreply.github.com>
2024-11-30 02:23:07 +0300

    Update config.js

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ module.exports = {
     development: {
         username: 'postgres1',
         password: 'L6mExxWlGa8xCDy6hjJ2UswXbLbrlBUS',
-        database: 'postgres_vp6u',
+        database: 'postgres',
         host: 'dpg-ct5484alqhvc73a6useg-a',
         port: 5432
     }
