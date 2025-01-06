0bf71646e39493581b55dba1714323af051ac44f
silivestir <84616833+silivestir@users.noreply.github.com>
2025-01-06 13:36:52 +0300

    Update dbConf.js
    i addded connection string to  sequelize

--- config/dbConf.js
+++ config/dbConf.js
@@ -1,14 +1,17 @@
 const { Sequelize } = require("sequelize");
 const config = require("./../config/config").development;
 require("dotenv").config(); // Ensure the environment variables are loaded
-
+/*
 // Initialize sequelize with environment variables
 const sequelize = new Sequelize(config.database, config.username, config.password, {
   host: config.host,
   port: config.port,
   dialect: 'postgres',
   logging: false, // Disabling Sequelize logging
-});
+});*/
+
+const sequelize = new Sequelize('postgres://bgckrxlt:OQ2TG25MyY8LMSy-NXsSaY4pLGNxXmTy@salt.db.elephantsql.com/bgckrxlt') // Example for postgres
+
 
 // Test the connection (optional but recommended for debugging)
 sequelize.authenticate()
